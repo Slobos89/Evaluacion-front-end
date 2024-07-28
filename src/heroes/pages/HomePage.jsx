a6cfd814a47b6e9fd453c63e50eecb6f35954d26
Sebastian <seba.lobos1989@gmail.com>
2024-07-28 04:50:55 -0400

    agregado descripcion de home

--- src/heroes/pages/HomePage.jsx
+++ src/heroes/pages/HomePage.jsx
@@ -1,6 +1,16 @@
+import "./style.css"
 
 export const HomePage = () => {
   return (
-    <div>HomePage</div>
+    <article className="margen">
+      <div>
+      <h1>Página Home</h1>
+      <p>Bienvenido a nuestro sitio web! Aquí encontrarás las siguientes secciones:</p>
+      <h4>Anime</h4>
+      <p>Sumérgete en el fascinante mundo del anime. Explora personajes icónicos, descubre sus historias y encuentra tu próximo anime favorito.</p>
+      <h4>Comics</h4>
+      <p>Adéntrate en el universo de los cómics. Conoce a tus superhéroes preferidos, descubre nuevos personajes y sigue sus emocionantes aventuras.</p>
+      </div>
+    </article>
   )
 }
